bd6938d95bb55e42e156a39a3d047d27332b90c0
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-09-14 09:58:58 -0400

    have data flowing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import axios from "axios";
 
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await axios.get(
+        `https://products-api-01.herokuapp.com/api/products`
+      );
+      setProducts(response.data);
+    };
+    getProducts();
+  }, [])
+
   return (
     <div className="App">
 
