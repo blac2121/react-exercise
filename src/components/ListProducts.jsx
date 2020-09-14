38802c88c024f047fd37594e82c85bb0f98754f6
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-09-14 11:47:57 -0400

    styled list view and got search working

--- src/components/ListProducts.jsx
+++ src/components/ListProducts.jsx
@@ -1,9 +1,57 @@
 import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
+const ProductMainContainter = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 1450px;
+  margin: 0 auto;
+`
+const ProductContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 350px;
+  max-height: 450px;
+  margin: 10px;
+  border: 1px solid #E4E6E6;
+  box-shadow: 1px 1px 1px 1px #E4E6E6;
+  align-items: center;
+  padding: 20px;
+  justify-content: space-evenly;
+  transition: all .2s ease-in-out; 
+
+  &:hover {
+    transform: scale(1.01); 
+  }
+`
+
+const ProductImg = styled.img`
+  display: block;
+  width: 300px;
+  height: 300px;
+`
+
+const ProductName = styled.h3`
+  font-size: 24px;
+  font-weight: 100;
+  color: black;
+  padding: 10px;
+  margin: 0;
+`
+const ProductPrice = styled.h3`
+  font-size: 28px;
+  margin: 0;
+  font-weight: 100;
+  color: black;
+  padding: 10px;
+`
 
 const ListProducts = (props) => {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState(null);
+
+  // search.filter(input => input === input.name)
+
 
   return (
     <div>
@@ -18,21 +66,25 @@ const ListProducts = (props) => {
       <div className="sort">
 
       </div>
-      <div className="products-container">
-        {props.products.map((product, index) => {
+      <ProductMainContainter>
+        {props.products.filter((product) => {
+          if (search === null)
+            return product
+          else if (product.name.includes(search)) 
+            return product           
+          })
+        .map((product, index) => {
           return (
-            <div key={index}>
-              <Link to={`/products/${product._id}`}>
-                <div key={product._id}>
-                  <img src={product.imgURL} />
-                  <h3>{product.name}</h3>
-                  <p>${product.price}</p>
-                </div>
-              </Link>
-            </div>  
+            <Link to={`/products/${product._id}`} key={index}>
+              <ProductContainer key={product._id}>
+                <ProductImg src={product.imgURL} />
+                <ProductName>{product.name}</ProductName>
+                <ProductPrice>${product.price}</ProductPrice>
+              </ProductContainer>
+            </Link> 
           )
         })}
-      </div>
+      </ProductMainContainter>
     </div>
 
 
--- src/components/ViewProduct.jsx
+++ src/components/ViewProduct.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
 
 
 const ViewProduct = (props) => {
